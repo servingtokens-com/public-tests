c57914fce98b554fd1e6fb462c5e78d973fcb477
divops.network <92889629+divopsresources@users.noreply.github.com>
2022-07-02 07:26:05 -0700

    Create unzip-from-source.sh

---
+++ unzip-from-source.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env import
+
+# demo name = > username-with-env-variables-example
+# docs link = > https://servingtokens.com/docs/functions/auth/authenticating-users/example-api/username-with-env-variables-example.sh
+# live exmaple = > https://www.sts-function-demos.servingtokens.com/api/username-with-env-variables-example.sh
+# live example = > https://sts-core-runtime-app-with-docker-1142.vercel.app/api/unzip-from-source
+# github repo => https://github.com/servingtokens-tests/documented-tests/new/main
+
+#!/bin/bash
+set -euo pipefail
+
+# `import` debug logs are always enabled during build
+export IMPORT_DEBUG=1
+export IMPORT_CURL_OPTS="-s -H"
+export IMPORT_RELOAD=0
+
+IMPORT_LIB="$IMPORT_CACHE/bin/lib"
+lastcmd="| last cmd = $? |" 
+
+handler() {
+    echo "Installing static \`package\` binary to \"$IMPORT_LIB\"" \
+    eval "$(curl -sfLS \"https://github.com/jwerre/node-canvas-lambda/blob/master/node12_canvas_lib64_layer.zip?raw=true\" > $IMPORT_LIB && unzip -o $IMPORT_LIB $IMPORT_LIB/lib &&  rm -rf $IMPORT_LIB/lib.zip:)" \
+    echo "$lastcmd" \
+    echo "sourcing lib \`package\` and removed the zip file" \
+    eval "$(. "$IMPORT_LIB/lib")" \
+    echo "$lastcmd" \
+    echo "listing the contents of the IMPORT_LIB dir" \
+    echo "$(ls -laiR $IMPORT_CACHE/bin)" \
+    echo "$lastcmd" \
+    echo "listing the contents of the IMPORT_LIB dir"
+}
